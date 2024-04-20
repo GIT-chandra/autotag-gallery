5fc9744e64d4340d96865ca1e1e0c33f2d91683d
Angom Geetchandra <angomgeetchandra@gmail.com>
2024-04-20 18:23:50 +0530

    remove desktop app components

--- garo.py
+++
@@ -1,9 +0,0 @@
-from garo_config import GaroConfig
-from garo_ui import GaroApp
-from garo_core import GaroCoreDummy
-
-cfg = GaroConfig()
-core = GaroCoreDummy(cfg.gallery_paths[0])
-print(vars(cfg))
-app = GaroApp(core, cfg)
-app.run()
